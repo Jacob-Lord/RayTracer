bf0b390ec543fa63cc2bf5a939f79caa74e79336
Jacob Lord <jacoblord02@hotmail.com>
2024-06-11 22:12:45 -0600

    added scaling() tests and function

--- src/libs/transformations.feature.js
+++ src/libs/transformations.feature.js
@@ -1,16 +1,31 @@
 import { point } from "./tuples.feature.js";
 
-
-
 export function translation(x, y, z) {
+    //define the 4x4 identity matrix
     let identity_matrix = [[1, 0, 0, 0],
     [0, 1, 0, 0],
     [0, 0, 1, 0],
     [0, 0, 0, 1]];
 
+    //translation respectively affects the far right column of the matrix
     identity_matrix[0][3] += x;
     identity_matrix[1][3] += y;
     identity_matrix[2][3] += z;
     
     return identity_matrix;
+}
+
+export function scaling(x, y, z) {
+    //define the 4x4 identity matrix
+    let identity_matrix = [[1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]];
+
+    //scaling affects the matrix diagonally from top left to bottom right
+    identity_matrix[0][0] = x;
+    identity_matrix[1][1] = y;
+    identity_matrix[2][2] = z;
+
+    return identity_matrix;     
 }
--- tests/transformations.feature.test.js
+++ tests/transformations.feature.test.js
@@ -1,6 +1,6 @@
 import {describe, test, it, expect} from "vitest";
 import { point, vector } from "../src/libs/tuples.feature";
-import { translation } from "../src/libs/transformations.feature";
+import { scaling, translation } from "../src/libs/transformations.feature";
 import { inverse, multiplyMatrix } from "../src/libs/matrices.feature";
 
 describe(translation, () => {
@@ -21,5 +21,32 @@ describe(translation, () => {
         let transform = translation(5, -3, 2);
         let v = vector(-3, 4, 5);
         expect(multiplyMatrix(transform, v)).toStrictEqual(v);
+    });
+})
+
+describe(scaling, () => {
+    it('scaling matrix applied to a point should result in point', () => {
+        let transform = scaling(2, 3, 4);
+        let p = point(-4, 6, 8);
+        expect(multiplyMatrix(transform, p)).toStrictEqual(point(-8,18,32));
+    });
+
+    it('scaling matrix applied to a vector should result in a vector', () => {
+        let transform = scaling(2, 3, 4);
+        let v = vector(-4, 6, 8);
+        expect(multiplyMatrix(transform, v)).toStrictEqual(vector(-8,18,32));
+    });
+
+    it('multiplying by inverse of scaling matrix', () => {
+        let transform = scaling(2, 3, 4);
+        let inv = inverse(transform);
+        let v = vector(-4, 6, 8);
+        expect(multiplyMatrix(inv, v)).toStrictEqual(vector(-2, 2, 2));
+    });
+
+    it('scaling by a negative value should result in refleciton', () => {
+        let transform = scaling(-1, 1, 1);
+        let p = point(2, 3, 4);
+        expect(multiplyMatrix(transform, p)).toStrictEqual(point(-2, 3, 4));
     });
 })
