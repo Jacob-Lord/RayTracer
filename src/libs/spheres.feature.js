5f98c9aa5c7c2a71f41603181c3fa6ed9ccd50b5
Jacob Lord <jacoblord02@hotmail.com>
2024-06-18 19:55:29 -0600

    added intersect() function

--- src/libs/spheres.feature.js
+++ src/libs/spheres.feature.js
@@ -1,7 +1,33 @@
-
+import { dot, point, subtractTuples } from "./tuples.feature.js";
 
 export class Sphere {
     constructor(id) {
         this.id = id;
+    }
+}
+
+export function intersect(sphere, ray) {
+    //distance of sphere from origin
+    let sphere_to_ray = subtractTuples(ray.origin, point(0, 0, 0));
+    let a = dot(ray.direction, ray.direction);
+    let b = 2 * dot(ray.direction, sphere_to_ray);
+    let c = dot(sphere_to_ray, sphere_to_ray) - 1;
+
+    let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+    //if ray misses sphere
+    if (discriminant < 0) {
+        return [];
+    }
+
+    let t1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    let t2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+
+    //ensure values are returned in ascending order for future access
+    if (t1 < t2) {
+        return [t1, t2];
+    }
+    else {
+        return [t2, t1];
     }
 }
--- tests/spheres.feature.test.js
+++ tests/spheres.feature.test.js
@@ -3,12 +3,12 @@ import { point, vector } from "../src/libs/tuples.feature";
 import { intersect, Sphere } from "../src/libs/spheres.feature";
 import { Ray } from "../src/libs/rays.feature";
 
-describe(sphere, () => {
+describe(Sphere, () => {
     it('should have the ray intersect the sphere at two points', () => {
         let r = new Ray(point(0,0,-5), vector(0,0,1));
         let s = new Sphere(1);
         let xs = intersect(s, r);
-        expect(xs.count).toBe(2);
+        expect(xs.length).toBe(2);
         expect(xs[0]).toBe(4.0);
         expect(xs[1]).toBe(6.0);
     });
@@ -17,7 +17,7 @@ describe(sphere, () => {
         let r = new Ray(point(0,1,-5), vector(0,0,1));
         let s = new Sphere(2);
         let xs = intersect(s, r);
-        expect(xs.count).toBe(2);
+        expect(xs.length).toBe(2);
         expect(xs[0]).toBe(5.0);
         expect(xs[1]).toBe(5.0);
     });
@@ -26,14 +26,14 @@ describe(sphere, () => {
         let r = new Ray(point(0,2,-5), vector(0,0,1));
         let s = new Sphere(3);
         let xs = intersect(s, r);
-        expect(xs.count).toBe(0);
+        expect(xs.length).toThrowError;
     });
 
     it('should originate inside a sphere', () => {
         let r = new Ray(point(0,0,0), vector(0,0,1));
         let s = new Sphere(2);
         let xs = intersect(s, r);
-        expect(xs.count).toBe(2);
+        expect(xs.length).toBe(2);
         expect(xs[0]).toBe(-1.0);
         expect(xs[1]).toBe(1.0);
     });
@@ -42,7 +42,7 @@ describe(sphere, () => {
         let r = new Ray(point(0,0,5), vector(0,0,1));
         let s = new Sphere(2);
         let xs = intersect(s, r);
-        expect(xs.count).toBe(2);
+        expect(xs.length).toBe(2);
         expect(xs[0]).toBe(-6.0);
         expect(xs[1]).toBe(-4.0);
     });
